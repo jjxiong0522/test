e9b2fb6e184c696b7cb28cc210f020fe2e28aff0
paas <paas@cse-codegen-backend-4053950211-v12gr>
2017-12-15 06:58:56 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/cse/controller/CseDelegate.java
@@ -0,0 +1,14 @@
+package com.service.cse.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CseDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
